cbd490d62ad7c9f1d10803ebde42a9c29d307a53
pouradvice <josh@pouradvice.com>
2026-03-16 15:09:41 -0500

    Refactor import from '@/lib/supabase/service' to '@/lib/supabase/server'

---
+++ path/to/modifiedFile.js
@@ -0,0 +1,4 @@
+// This file has been modified to change imports from '@/lib/supabase/service' to '@/lib/supabase/server'.
+import createServiceClient from '@/lib/supabase/server';
+
+// other existing code
